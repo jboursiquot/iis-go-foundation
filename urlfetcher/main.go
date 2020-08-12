531fe127fc2850e0b40619741c6a144451f32e9b
Johnny Boursiquot <jboursiquot@gmail.com>
2020-08-12 15:58:37 -0400

    Minor update to urlfetcher

--- urlfetcher/main.go
+++ urlfetcher/main.go
@@ -11,12 +11,14 @@ func main() {
 	for _, url := range os.Args[1:] {
 		resp, err := http.Get(url)
 		if err != nil {
+			fmt.Println(err)
 			os.Exit(1)
 		}
 
 		b, err := ioutil.ReadAll(resp.Body)
 		resp.Body.Close()
 		if err != nil {
+			fmt.Println(err)
 			os.Exit(1)
 		}
 		fmt.Printf("%s\n\n", url)
